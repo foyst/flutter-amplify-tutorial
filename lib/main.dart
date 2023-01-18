778cde37fc1a473c4c9a771003e8c70916694bd7
Ben Foster <benfoster88@hotmail.com>
2023-01-18 10:00:46 +0000

    Improved UI design styling

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyAppState extends State<MyApp> {
           // is not restarted.
           primarySwatch: Colors.blue,
           useMaterial3: true),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Amplify Quickstart'),
       builder: Authenticator.builder(),
     ));
   }
@@ -131,61 +131,79 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        child: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'User Profile',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const ProfilePicture(),
-              TextField(
-                decoration: const InputDecoration(labelText: "Name"),
-                controller: _nameController,
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: "Location"),
-                controller: _locationController,
-              ),
-              TextField(
-                decoration:
-                    const InputDecoration(labelText: "Favourite Language"),
-                controller: _languageController,
-              )
-            ],
-          ),
+    return Stack(
+      children: [
+        Image.asset(
+          "assets/background.jpg",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateUserDetails,
-        tooltip: 'Save Details',
-        child: const Icon(Icons.save),
-      ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: Text(widget.title),
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.white,
+          ),
+          body: Center(
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    // Column is also a layout widget. It takes a list of children and
+                    // arranges them vertically. By default, it sizes itself to fit its
+                    // children horizontally, and tries to be as tall as its parent.
+                    //
+                    // Invoke "debug painting" (press "p" in the console, choose the
+                    // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                    // to see the wireframe for each widget.
+                    //
+                    // Column has various properties to control how it sizes itself and
+                    // how it positions its children. Here we use mainAxisAlignment to
+                    // center the children vertically; the main axis here is the vertical
+                    // axis because Columns are vertical (the cross axis would be
+                    // horizontal).
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: Text(
+                          'User Profile',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        )),
+                        const ProfilePicture(),
+                        TextField(
+                          decoration: const InputDecoration(labelText: "Name"),
+                          controller: _nameController,
+                        ),
+                        TextField(
+                          decoration:
+                              const InputDecoration(labelText: "Location"),
+                          controller: _locationController,
+                        ),
+                        TextField(
+                          decoration: const InputDecoration(
+                              labelText: "Favourite Language"),
+                          controller: _languageController,
+                        )
+                      ],
+                    ),
+                  ))),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _updateUserDetails,
+            tooltip: 'Save Details',
+            child: const Icon(Icons.save),
+          ),
+        )
+      ],
     );
   }
 
